23ade58288106badc3741c4c476f9f83a45b44f6
Riley <riley.s2003@hotmail.com>
2025-11-14 21:33:58 -0600

    some of post functionality

--- assets/scripts/Subscriber.js
+++ assets/scripts/Subscriber.js
@@ -2,12 +2,13 @@
 
 import { User } from './User.js';
 
-export class Subscriber extends User{
+export class Subscriber extends User {
     #pages;
     #groups;
     #canMonetize;
 
     constructor(id, name, userName, email, pages, groups, canMonetize){
+        super(id, name, userName, email);
         this.#pages = pages;
         this.#groups = groups;
         this.#canMonetize = canMonetize;
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,6 +11,7 @@ let profilePic = select('#profile-pic');
 let modalName = select('#modal-name');
 let modalContent = select('.modal-content');
 let modalPicture = select('.modal-picture');
+let postsContainer = select('.post-flex-container');
 
 let modalIsOpen = false;
 
@@ -19,10 +20,26 @@ const subscriber = new Subscriber (
 )
 
 function openModal(){
+    console.log('hello');
     modalIsOpen = true;
     modal.classList.remove('hidden');
     modalContent.classList.remove('hidden');
     modalPicture.classList.remove('hidden');
 }
 
+function post() {
+    if(textInput.value.trim() !== "") {
+        let post = document.createElement('div');
+        post.classList.add('post-container');
+        postsContainer.appendChild(post);
+        
+        let postText = document.createElement('p');
+        postText.value = textInput;
+        postText.appendChild(post);
+    }
+
+    // return;
+}
+
 if(!modalIsOpen) listen('click', profilePic, openModal);
+listen('click', postButton, post);
